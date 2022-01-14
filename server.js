4488ab6d3ee0012304873f3d9ba306135a1369a2
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-01-13 22:18:52 -0500

    added user prompts and db and schema sql

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE,
+    }
+);
+
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        }
+    ])
+};
